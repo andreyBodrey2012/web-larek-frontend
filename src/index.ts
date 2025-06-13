e3a9e479349e936f6bfede82ec8add239abece17
Andrey Zapozdaev <andrejzapozdaev@gmail.com>
2025-06-13 14:00:06 +0300

    Upd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
 import './scss/styles.scss';
-// import { HomePageView } from './types/views'
-// import { EventEmitter } from './types/events'
-
-
-// const eventEmitter = new EventEmitter()
-
-// const homePage = new HomePageView(eventEmitter, document.querySelector('.page'))
